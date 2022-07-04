b93ae118ec9cda8a050ab820d56becb0e479796e
Sergey <thextreme.30@gmail.com>
2022-07-04 20:14:22 +0600

    created blog app

--- personal_portfolio/urls.py
+++ personal_portfolio/urls.py
@@ -1,14 +1,14 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 
-from personal_portfolio.settings import MEDIA_ROOT
 from portfolio import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='home '),
+    path('', views.home, name='home'),
+    path('blog/', include('blog.urls')),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
